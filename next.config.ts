ada66e5e6a938096e4dbcafd3e3a6f862ddd05e3
irfanullahdhark <irfanullahdhark@gmail.com>
2025-11-05 20:18:57 +0430

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 
   images: {
-    domains: ['images.unsplash.com','images.pexels.com', 'cdn-icons-png.flaticon.com','cdn.pixabay.com',
-       'commondatastorage.googleapis.com', 'sample-videos.com', 'randomuser.me','source.unsplash.com',
-       'storyset.com', 'img.freepik.com'], // allow Storyset/Freepik assets
+      domains: [
+        'images.unsplash.com',
+        'images.pexels.com', 
+        'cdn-icons-png.flaticon.com',
+        'cdn.pixabay.com',
+        'commondatastorage.googleapis.com', 
+        'sample-videos.com', 
+        'randomuser.me',
+        'source.unsplash.com',
+        'storyset.com',
+        'img.freepik.com',
+        '192.168.1.72' // Use only the hostname, not the port
+      ], // allow Storyset/Freepik assets
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: '192.168.1.72',
+          port: '8005',
+        },
+      ],
   },
   // i18n is removed if using App Router
 
